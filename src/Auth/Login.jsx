ecb55d7bcfe552983b7aebec5ffd719bcb8c8855
Felix A. Taveras <felixataveras1@gmail.com>
2023-12-21 14:47:03 -0500

    adding css

--- src/Auth/Login.jsx
+++ src/Auth/Login.jsx
@@ -1,20 +1,41 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useAuth0 } from '@auth0/auth0-react';
 import { Button } from 'react-bootstrap';
+import '../../src/index.css';
+
+const rotatingWords = ['Hard', "Can't", 'Later', 'Ease'];
 
 function Login() {
-
-  const {
-    isAuthenticated,
-    loginWithRedirect,
-  } = useAuth0();
+  const { isAuthenticated, loginWithRedirect } = useAuth0();
+  const [index, setIndex] = useState(0);
 
   function handleLogin() {
     loginWithRedirect();
   }
 
-  return !isAuthenticated &&
-    <Button onClick={handleLogin}>Log in</Button>
-    ;
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setIndex(prevIndex => (prevIndex + 1 >= rotatingWords.length ? 0 : prevIndex + 1));
+    }, 1000);
+
+    return () => clearInterval(interval);
+  }, []);
+
+  return (
+    <div className="login-page">
+      {!isAuthenticated && (
+        <div className="rotating-words-container" id="rotating-words">
+          <span id="goal-text">Goal</span>
+          <span id="rotating-word">
+            {rotatingWords[index]}
+          </span>
+          <Button onClick={handleLogin} className="login-button">
+            Log in
+          </Button>
+        </div>
+      )}
+    </div>
+  );
 }
+
 export default Login;
--- src/components/NewHeader.jsx
+++ src/components/NewHeader.jsx
@@ -1,26 +1,55 @@
 import React from 'react';
 import { Link } from "react-router-dom";
-import Container from 'react-bootstrap/Container';
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
+import { Navbar, Nav, NavDropdown } from 'react-bootstrap';
+import { FaSignInAlt, FaSignOutAlt } from 'react-icons/fa';
+import { useSelector } from 'react-redux';
 
 class NewHeader extends React.Component {
     render() {
+        // Assuming useSelector can be used here. If not, consider other ways to access Redux state.
+        const user = useSelector((state) => state.auth.user);
+
+        const onLogout = () => {
+            // Implement logout functionality
+        };
+
         return (
-            <>
-                <Navbar bg="dark" data-bs-theme="dark">
-                    <Container>
-                        <Navbar.Brand><Link to="/" className="nav-link">GoalEase</Link></Navbar.Brand>
+            <Navbar bg="dark" expand="lg" fixed="top" className="px-4 px-md-4 shadow-lg">
+                <div className="d-flex justify-content-between align-items-center w-100">
+                    <Navbar.Brand as={Link} to="/" className="nav-brand">
+                        GoalEase
+                    </Navbar.Brand>
+                    <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                    <Navbar.Collapse id="basic-navbar-nav">
                         <Nav className="me-auto">
-                            <Navbar.Brand><Link to="/" className="nav-link">Home</Link></Navbar.Brand>
-                            <Navbar.Brand><Link to="/goalForm" className="nav-link">Goal Form</Link></Navbar.Brand>
-                            <Navbar.Brand><Link to="/snakeGame" className="nav-link">Snake Game</Link></Navbar.Brand>
-                            <Navbar.Brand><Link to="/about" className="nav-link">About</Link></Navbar.Brand>
+                            <Nav.Link className="nav-words" as={Link} to="/">Home</Nav.Link>
+                            <Nav.Link className="nav-words" as={Link} to="/about">About</Nav.Link>
+                            <NavDropdown
+                                className="nav-words"
+                                title="Options"
+                                id="basic-nav-dropdown"
+                            >
+                                <NavDropdown.Item as={Link} to="/newGoal">Make A New Goal</NavDropdown.Item>
+                                <NavDropdown.Item as={Link} to="/snakeGame">Play Snake Game</NavDropdown.Item>
+                                <NavDropdown.Item as={Link} to="/publicComment">Make A Public Comment</NavDropdown.Item>
+                            </NavDropdown>
                         </Nav>
-                    </Container>
-                </Navbar>
-            </>
+                        <Nav className="ml-auto">
+                            {user ? (
+                                <Nav.Link className="nav-words" onClick={onLogout}>
+                                    <FaSignOutAlt /> Logout
+                                </Nav.Link>
+                            ) : (
+                                <Nav.Link as={Link} to="/login" className="nav-words">
+                                    <FaSignInAlt /> Login
+                                </Nav.Link>
+                            )}
+                        </Nav>
+                    </Navbar.Collapse>
+                </div>
+            </Navbar>
         );
     }
 }
+
 export default NewHeader;
