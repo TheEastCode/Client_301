63ba6ae7413b9209a640f89dff4d5f99fd370e10
Latherio Kidd <kidd.latherio@gmail.comS>
2023-12-22 02:37:58 -0500

    pull chnages from main

---
+++ src/components/Snake.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+
+function MyIframeComponent() {
+    return (
+      <div>
+        <iframe
+          src="https://theeastcode.github.io/ClassicSnakeGame/"
+          width="500"
+          height="300"
+          title="Embedded Page"
+        ></iframe>
+      </div>
+    );
+  }
+  export default MyIframeComponent;
